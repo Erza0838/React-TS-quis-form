7554454e56d79828d610b66066b0f170d7ebd59c
muhammaderza884@gmail.com <muhammaderza884@gmail.com>
2024-09-21 22:17:41 +0700

    Progres-1 login provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,7 @@
-import NextAuth from "next-auth/next";
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth/next"
+// import { authOptions } from "../../../../../lib/authOptions"
+import authOptions from "../../../../../lib/authOptions"
 
-export const handler = NextAuth({
-  providers:[
-    GithubProvider({
-        clientId: process.env.GITHUB_ID as string,
-        clientSecret: process.env.GITHUB_SECRET as string
-    }),
-    GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-    })
-  ]
-})
+const handler = NextAuth(authOptions) 
+
+export {handler as GET, handler as POST}
--- src/components/LoginFormComponent.tsx
+++ src/components/LoginFormComponent.tsx
@@ -1,19 +1,34 @@
+"use client"
 import React from "react"
-import { useSession } from "next-auth/react"
-
+import { signOut, useSession } from "next-auth/react"
+import { signIn } from "next-auth/react"
 
 export default function LoginFormComponent() 
 {
-  const {data: session } = useSession()
+  const {data: session } = useSession() 
   return (
     <>
         {session ? (
             <>
-                <h1>Halo</h1>
+                {/* <img src={session.user?.image as string} className="rounded-full h-10"/> */}
+                <div className="flex flex-col">
+                    <h1 className="text-black">{session.user?.name}</h1>
+                    <h1 className="text-black">{session.user?.email}</h1>
+                    <button className="border border-red-600 rounded-lg text-red-600 w-16"
+                    onClick={() => signOut()}>Logout</button>
+                </div>
             </>
         ) : (
             <>
-            
+                <div className="flex justify-center">
+                    <div className="flex flex-col">
+                        <h1 className="text-red-500 font-bold">Anda belum login</h1>
+                        <div className="flex flex-row gap-5">
+                            <button onClick={() => signIn("google")} className="border border-black rounded-lg text-black w-16">Login Google</button>
+                            <button onClick={() => signIn("github")} className="border border-black rounded-lg text-black w-16">Login github</button>
+                        </div>
+                    </div>
+                </div>
             </>
         )}
     </>
